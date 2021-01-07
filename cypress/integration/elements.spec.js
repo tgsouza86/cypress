8e03a43c7d5a378d0d01976276f7a3531323dc3d
Tiago Gonçalves <tgsouza86@gmail.com>
2021-01-07 10:50:26 -0300

    Combos

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -77,11 +77,27 @@ describe('Work with basic elements', () => {
         .select('1graucomp')
         .should('have.value', '1graucomp')
 
+        cy.get('[data-test=dataEscolaridade] option').should('have.length', 8)
+        cy.get('[data-test=dataEscolaridade] option').then($arr => {
+            const values = []
+            $arr.each(function() {
+                values.push(this.innerHTML)
+            })
+            expect(values).to.include.members(["Superior", "Mestrado"])
+        })
     })
 
     it.only('Combo multiplo', () => {
         cy.get('[data-testid=dataEsportes]')
         .select(['natacao', 'Corrida', 'nada'])
+
+     // cy.get('[data-testid=dataEsportes]').should('have.values', ['natacao', 'Corrida', 'nada'])
+        cy.get('[data-testid=dataEsportes]').then($el => {
+           expect($el.val()).to.be.deep.equal(['natacao', 'Corrida', 'nada'])
+           expect($el.val()).to.have.length(3)
+        })
+        cy.get('[data-testid=dataEsportes]').invoke('val')
+        .should('eql', ['natacao', 'Corrida', 'nada'])
     })
 })
 
