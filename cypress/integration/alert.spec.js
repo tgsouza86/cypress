dacd3b0118cdb14e4d4661cd242a8deaa10e03d7
Tiago Gonçalves <tgsouza86@gmail.com>
2021-01-07 11:29:59 -0300

    Alert

---
+++ cypress/integration/alert.spec.js
@@ -0,0 +1,21 @@
+///<reference types="cypress" />
+
+describe('Work with alerts', () => {
+   
+    before(() => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html') 
+    })
+
+    beforeEach(() => {
+        cy.reload()
+    })
+
+    it('Alert', () => {
+        cy.get('#alert').click()
+        cy.on('window:alert', msg => {
+            console.log(msg)
+            expect(msg).to.be.equal('Alert Simples')
+        })
+    })
+
+})
