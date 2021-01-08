87727d24962d2c467fbac5a125c04883035db70b
Tiago Gonçalves <tgsouza86@gmail.com>
2021-01-08 10:33:24 -0300

    Stub

--- cypress/integration/alert.spec.js
+++ cypress/integration/alert.spec.js
@@ -18,4 +18,15 @@ describe('Work with alerts', () => {
         })
     })
 
+    it.only('Alert com mock', () => {
+
+        const stub = cy.stub().as('alerta')
+        
+        cy.on('window:alert', stub)
+            cy.get('#alert').click().then(() => {
+                expect(stub.getCall(0)).to.be.calledWith('Alert Simples')
+            })
+      
+    })
+
 })
