17b4c7fb752640c2bd3b80e6d5e34798cc6a5779
Tiago Gonçalves <tgsouza86@gmail.com>
2021-01-13 14:45:27 -0300

    iFrame part 1

---
+++ cypress/integration/iframe.spec.js
@@ -0,0 +1,19 @@
+///<reference types="cypress" />
+
+describe('Work with iFrames', () => {
+   
+    before(() => {
+         
+    })
+
+    it('Deve preencher campo de texto', () => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+        cy.get('#frame1').then(iframe => {
+            const body = iframe.contents().find('body')
+            cy.wrap(body).find('#tfield')
+                .type('funciona?')
+                .should('have.value', 'funciona?')
+        })
+
+    })
+})
