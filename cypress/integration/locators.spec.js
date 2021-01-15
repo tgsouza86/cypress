058dc8ff5054e94e4ef96dd8f185c9aa2dc53328
Tiago Gonçalves <tgsouza86@gmail.com>
2021-01-14 22:02:42 -0300

    Locators

--- cypress/integration/locators.spec.js
+++ cypress/integration/locators.spec.js
@@ -10,7 +10,12 @@ describe('Work with basic elements', () => {
         cy.reload()
     })
 
-    it ('......', () => {
+    it ('using jquery selector ', () => {
+        cy.get('table#tabelaUsuarios tbody>tr:eq(0) td:nth-child(3)>input').click()
+        ///cy.get([onclick*='\Francisco\'])
+           cy.get("[onclick*='\Francisco\']")
+           cy.get('#tabelaUsuarios td:contains(\'Doutorado\'):eq(0)~ td:eq(3) > input')
+           cy.get('#tabelaUsuarios tr:contains(\'Doutorado\'):eq(0) td:eq(6) input')
 
     })
 })
