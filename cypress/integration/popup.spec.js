934503c9b14c3b2cade5e7da8915ace29f7c8e45
Tiago Gonçalves <tgsouza86@gmail.com>
2021-01-14 21:01:05 -0300

    Popup with links

--- cypress/integration/popup.spec.js
+++ cypress/integration/popup.spec.js
@@ -21,5 +21,31 @@ describe('Work with Popup', () => {
 
     })
 
+    describe.only('With links... ', () => {
+        beforeEach(() => {
+            cy.visit('https://wcaquino.me/cypress/componentes.html')
+        })
+        it('Check popup url', () => {
+            cy.contains('Popup2')
+            .should('have.prop', 'href')
+            .and('equal', 'https://wcaquino.me/cypress/frame.html')
 
+        })
+
+        it('Should acess popup dinamically', () => {
+            cy.contains('Popup2').then($a => {
+                const href = $a.prop('href')
+                cy.visit(href)
+                cy.get('#tfield').type('funciona')
+            })
+
+        })
+        it('Should force link on same page', () => {
+            cy.contains('Popup2')
+                .invoke('removeAttr', 'target')
+                .click()
+                cy.get('#tfield').type('funciona')
+        
+        })
+    })
 })
