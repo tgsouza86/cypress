ac5a8d3fb6c6b552a870f557f8c3e43f3f3da184
Tiago Gonçalves <tgsouza86@gmail.com>
2021-01-14 21:19:55 -0300

    Search Order

---
+++ cypress/integration/locators.spec.js
@@ -0,0 +1,16 @@
+///<reference types="cypress" />
+
+describe('Work with basic elements', () => {
+   
+    before(() => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html') 
+    })
+
+    beforeEach(() => {
+        cy.reload()
+    })
+
+    it ('......', () => {
+
+    })
+})
