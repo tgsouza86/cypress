6f0941a0cb7a02539d35fdf274ed33708479d565
Tiago Gonçalves <tgsouza86@gmail.com>
2021-01-31 17:56:18 -0300

    Insert changed account

--- cypress/integration/barriga/functional.spec.js
+++ cypress/integration/barriga/functional.spec.js
@@ -15,7 +15,19 @@ describe('Should Test at a functional level ', () => {
         cy.get('[href="/contas"]').click()
         cy.get('[data-test=nome]').type('Conta de teste Tiago')
         cy.get('.btn').click()
-        cy.get('.toast-message').should('contain', 'inserida com sucesso')
+        cy.get('.toast-message').should('contain', 'Conta inserida com sucesso')
+    })
+
+    it('Should update an account', () => {
+       // cy.get(':nth-child(3) > :nth-child(2) > .fa-edit')
+       cy.get('[data-test=menu-settings]').click()
+       cy.get('[href="/contas"]').click()
+       cy.xpath("//table//td[contains(., 'Conta de teste Tiago')]/..//i[@class='far fa-edit']").click()
+       cy.get('[data-test=nome]')
+        .clear()
+        .type('Conta alterada')
+       cy.get('.btn').click()
+       cy.get('.toast-message').should('contain', 'Conta atualizada com sucesso') 
     })
 })
 
